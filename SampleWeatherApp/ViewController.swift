9b158bb4c1a83fe3a6d6080ecdb7c0299537c796
anna_lazar <anna.lazar@apagroup.pl>
2019-11-28 21:07:13 +0100

    Initial Commit

---
+++ SampleWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleWeatherApp
+//
+//  Created by Anna Lazar on 28/11/2019.
+//  Copyright © 2019 APA Sp. z o. o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
